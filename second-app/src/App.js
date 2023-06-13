ff5c8f087f5be8aa92579c409937072c9e7da004
Vaibhav <chavan.vaibhav27@gmail.com>
2023-06-13 09:16:48 +0530

    rules

--- second-app/src/App.js
+++ second-app/src/App.js
@@ -1,11 +1,19 @@
 // JSX=JS+HTML
 // if function returns HTML, it becomes user defined tag
 // User Defined tag
-// - First letter uppercase
+// - First letter uppercase <App></App>
 // predefined tag convention
-// - lower case
-function App() {
+// - lower case <h1></h1>
+function App() {            // returns HTML so first letter capital
  return <h1>Hello World</h1>
 }
 
 export default App;
+
+function sum(){ // doesnt return HTML so all small
+  return 100;
+}
+
+function Sum(){ // returns HTML first letter capital
+  return <h1>100</h1>;
+}
